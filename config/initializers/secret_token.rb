c1cc7c98c1b040569a1c87331672618f18180e9c
Aaron K. Hawkins <akh@aaronkhawkins.com>
2012-02-17 16:47:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whoohoo::Application.config.secret_token = '3d564b95f55242d5777da7073e09d7a9d96853e721773ef511ebb4e4331d37a7c233eb0b70dcc143077dfd857f20fefb65aab4228f410b395f43aefe7bd62de6'
